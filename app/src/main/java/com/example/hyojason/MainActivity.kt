d0caf6ad0306d1ff68c30af8d9441b39d2d48a23
Gyudol1231 <jgy123117@naver.com>
2023-12-15 05:32:13 +0900

    feat: subpage이동 구현

--- app/src/main/java/com/example/hyojason/MainActivity.kt
+++ app/src/main/java/com/example/hyojason/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hyojason
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // move to subactivity
+        val transitionButton: Button = findViewById(R.id.movePageButton)
+        transitionButton.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
