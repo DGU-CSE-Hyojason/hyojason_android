db3ef12f59042444885071fe6d65818ec3c4e111
leewoojye <whitehole36@naver.com>
2023-12-15 02:52:42 +0900

    listview 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.hyojason"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hyojason"
--- app/src/main/java/com/example/hyojason/SubActivity.kt
+++ app/src/main/java/com/example/hyojason/SubActivity.kt
@@ -3,19 +3,37 @@ package com.example.hyojason
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter;
+import android.widget.Button
 import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
+import android.view.View
+import android.content.Intent
+import android.widget.ImageButton
 
 class SubActivity : AppCompatActivity() {
 
-    var peopleList = arrayListOf<People>()
+    var peopleList = arrayListOf<People>(
+        People("이우제", "24세", "코딩"),
+        People("김태형", "24세", "커피"),
+        People("이하늘", "25세", "탈색"),
+        People("정규용", "23세", "컴파일러공부")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+        val mainListView: ListView = findViewById(R.id.mainListView)
 
         val peopleAdapter = MainListAdapter(this, peopleList)
         mainListView.adapter = peopleAdapter
+
+        // 홈으로 돌아가기 버튼
+        val transitionButton: ImageButton = findViewById(R.id.backButton)
+        transitionButton.setOnClickListener {
+            val intent = Intent(this@SubActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
